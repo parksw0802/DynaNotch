6e41509cfc83146505aaaae617872df6668d8aba
sewoongpark <qkrtpdnd0802@gmail.com>
2026-04-12 03:04:21 +0900

    feat: init DynaNotch Xcode project

---
+++ DynaNotch/DynaNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynaNotchApp.swift
+//  DynaNotch
+//
+//  Created by qkrtpdnd0802 on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct DynaNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
